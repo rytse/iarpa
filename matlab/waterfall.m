296414d5d44c078235aff6aaa07e2ba3a3981389
Ryan Tse <ryantse100@gmail.com>
2019-08-13 16:24:50 -0400

    Plot full waterfall (no trimming) of pulsed sounding

--- matlab/waterfall.m
+++ matlab/waterfall.m
@@ -1,13 +1,12 @@
-function waterfall(cin, fs)
+function waterfall(cin, fs, wsize)
 % Waterfall plot
 %   cin: input signal (complex)
 %   fs: sampling rate (Hz)
 
 ELAPSED_TIME = length(cin) / fs;
 
-% win = nuttallwin(65536)%/16/16);
-win = ones(65536/16/16, 1);
-% win = nuttallwin(8192);
+% win = nuttallwin(wsize);
+win = ones(wsize, 1);
 f = zeros(length(win),floor(length(cin)/length(win)/2), 'single');
 jj = 1;
 
@@ -18,10 +17,11 @@ end
 
 [k,m] = size(f);
 
-start_freq = 6043612 +29.7e6 + 49.3e6;
-alpha = 100e3;
+% start_freq = 6043612 +29.7e6 + 49.3e6;
+% alpha = 100e3;
 
-gr = imagesc(linspace(start_freq, start_freq + alpha * ELAPSED_TIME, m) / 1e6,linspace(0, fs / (fs*100) * 3e8 / 1000, k), f);
+gr = imagesc(linspace(0, ELAPSED_TIME, m) / 1e6, linspace(0, fs, k), f);
+% gr = imagesc(linspace(start_freq, start_freq + alpha * ELAPSED_TIME, m) / 1e6,linspace(0, fs / (fs*100) * 3e8 / 1000, k), f);
 % gr = imagesc(linspace(0, ELAPSED_TIME, m),linspace(0, 50e6 / 4, k), f(1:round(length(f)/4),:));
 % gr = imagesc(linspace(0, ELAPSED_TIME, m),linspace(0, fs / 4, k), f(1:round(length(f)/4),round(m / 2) : round(m / 2 * 1.3)));
 
