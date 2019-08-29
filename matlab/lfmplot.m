80a970a21c23076db9686833ca8ce2f32609d34c
Ryan Tse <rytse@protonmail.com>
2019-08-29 18:19:29 -0400

    Max pool spectrogram

--- matlab/lfmplot.m
+++ matlab/lfmplot.m
@@ -6,8 +6,14 @@ fid = fopen('../data/linear/train-000.bin');
 iq = fread(fid, FS * DUR * 2, 'int16');
 iq = fread(fid, FS * DUR * 2, 'int16');
 iq = fread(fid, FS * DUR * 2, 'int16');
-iq = fread(fid, FS * DUR * 2, 'int16');
+iq = fread(fid, FS * DUR * 2 * 2, 'int16');
 fclose(fid);
 iq = deinterleve(iq);
 
-waterfall(iq, FS, 65536/16);
+wf = waterfall(iq, FS, 65536/16);
+
+figure;
+pooled = blockproc(wf, [20 20], @(block_struct) max(block_struct.data));
+imagesc(linspace(0, 5, size(pooled, 1)), linspace(0, 20, size(pooled, 2)), pooled);
+set(gca,'YDir','normal')
+colormap(hsv);
