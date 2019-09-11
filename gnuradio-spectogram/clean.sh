75381349990562fbd72625418981c8fc3935b20d
Alex Kreidler <alexkreidler2020@gmail.com>
2019-09-11 12:24:24 -0400

    various fixes

--- gnuradio-spectogram/clean.sh
+++ gnuradio-spectogram/clean.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 env
+echo "done"
 
-# . ~/.bashrc
+. ~/.bashrc
 
 
-export GNU_RADIO_PATH=/miniconda3/envs/dsp
-export PATH=/opt/qt/bin:$PATH:/miniconda3/envs/dsp/bin
-export LD_LIBRARY_PATH=/opt/qt/lib:/usr/local/lib:$LD_LIBRARY_PATH:/miniconda3/envs/dsp/lib
-export PKG_CONFIG_PATH=/opt/qt/lib/pkgconfig:$PKG_CONFIG_PATH:/miniconda3/envs/dsp/lib/pkgconfig
-export PYTHONPATH=:/lib/python3.7/dist-packages
+# export GNU_RADIO_PATH=/miniconda3/envs/dsp
+# export PATH=/opt/qt/bin:$PATH:/miniconda3/envs/dsp/bin
+# export LD_LIBRARY_PATH=/opt/qt/lib:/usr/local/lib:$LD_LIBRARY_PATH:/miniconda3/envs/dsp/lib
+# export PKG_CONFIG_PATH=/opt/qt/lib/pkgconfig:$PKG_CONFIG_PATH:/miniconda3/envs/dsp/lib/pkgconfig
+# export PYTHONPATH=:/lib/python3.7/dist-packages
 
 env
 
